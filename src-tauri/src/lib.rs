118eef22cce050e3dae84b2970bd1fc7fc1c0a0d
lookfeeb <lookfeeb@hotmail.com>
2026-05-10 15:18:25 +0800

    fix: 修复 updater 端点编译时验证失败

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -41,7 +41,9 @@ pub fn run() {
         ))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_updater::Builder::new().build());
+        .plugin(tauri_plugin_updater::Builder::new()
+            .endpoints(vec!["https://github.com/lookfeeb/SysPulse/releases/latest/download/latest.json".into()])
+            .build());
 
     #[cfg(not(test))]
     let builder = builder
